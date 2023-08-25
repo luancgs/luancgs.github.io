0740190a86cda75626c114a3b2a93945db4ac569
Luan <luan.carlos.3174@gmail.com>
2023-08-25 01:12:18 -0300

    refactor: removes old structure files to give way for astro framework

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Luan', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Luan/i);
-  expect(linkElement).toBeInTheDocument();
-});
